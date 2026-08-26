73ab30b56d532e8082b5d391dd21363baa3e0fc7
SaikiroMurphy <ducanhn2810@gmail.com>
2026-08-27 02:12:48 +0700

    session_07

---
+++ session_07/cart-service/src/main/java/edu/cart_service/CartServiceApplication.java
@@ -0,0 +1,18 @@
+package edu.cart_service;
+
+import java.util.TimeZone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CartServiceApplication {
+
+	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+		SpringApplication.run(CartServiceApplication.class, args);
+	}
+
+}
